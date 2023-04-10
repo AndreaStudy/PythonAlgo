3ad2e7f769bd76e41b80972a0d89752243001106
Andrea <wjdgnstjr45@gmail.com>
2023-04-10 17:36:39 +0900

    20230410

--- baekjoon/gold/g5_14891.py
+++ baekjoon/gold/g5_14891.py
@@ -1,65 +1,55 @@
 import sys
 input = sys.stdin.readline
 
-def rightrotate(n):
+def rightrotate(n, d):
     if n == 4:
         return
     if cycle[n-1][2] != cycle[n][6]:
-        temp[n-1] = temp[n] = 1
-        rightrotate(n+1)
+        temp[n] = 1
+        direction[n] = -d
+        rightrotate(n+1, -d)
     return
 
-def leftrotate(n):
+def leftrotate(n, d):
     if n == 1:
         return
     if cycle[n-1][6] != cycle[n-2][2]:
-        temp[n-1] = temp[n-2] = 1
-        leftrotate(n-1)
+        temp[n-2] = 1
+        direction[n-2] = -d
+        leftrotate(n-1, -d)
     return
 
-def cal(d):
+def cal():
     for i in range(4):
         if temp[i]:
-            if i in (0,2):
-                if d == 1:
-                    tmp = [cycle[i].pop()]
-                    cycle[i] = tmp + cycle[i]
-                    print('1')
-                else:
-                    tmp = [cycle[i][0]]
-                    cycle[i] = cycle[i][1:]+tmp
-                    print('2')
+            copy_lst = list(cycle[i])
+            if direction[i] == 1:
+                cycle[i] = [copy_lst[-1]] + copy_lst[:7]
             else:
-                if d == 1:
-                    tmp = [cycle[i][0]]
-                    cycle[i] = cycle[i][1:]+tmp
-                    print('3')
-                else:
-                    tmp = [cycle[i].pop()]
-                    cycle[i] = tmp + cycle[i]
-                    print('4')
+                cycle[i] = copy_lst[1:] + [copy_lst[0]]
                 
-cycle = [list(input().rstrip()) for _ in range(4)]
+cycle = [list(map(int, input().rstrip())) for _ in range(4)]
 K = int(input())
 for _ in range(K):
-    temp = [0, 0, 0, 0]
     N, dir = map(int, input().split())
-    rightrotate(N)
-    leftrotate(N)
-    cal(dir)
-    for i in cycle:
-        print(*i)
+    temp = [0, 0, 0, 0]
+    direction = [0, 0, 0, 0]
+    temp[N-1] = 1
+    direction[N-1] = dir
+    rightrotate(N, dir)
+    leftrotate(N, dir)
+    cal()
     
 
 # 계산
 result = 0
-if cycle[0][0] == '1':
+if cycle[0][0] == 1:
     result += 1
-if cycle[1][0] == '1':
+if cycle[1][0] == 1:
     result += 2
-if cycle[2][0] == '1':
+if cycle[2][0] == 1:
     result += 4
-if cycle[3][0] == '1':
+if cycle[3][0] == 1:
     result += 8
     
 print(result)
