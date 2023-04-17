15e0e7e8a16f868bb07e777ca796bbff2a267cc7
Andrea <wjdgnstjr45@gmail.com>
2023-04-17 21:17:03 +0900

    20230417

--- baekjoon/silver/s1_11403.py
+++ baekjoon/silver/s1_11403.py
@@ -13,8 +13,4 @@ for k in range(N):
 for a in range(N):
     for b in range(N):
         print(arr[a][b], end=" ")
-        # if arr[a][b] == 0:
-        #     print(0, end=" ")
-        # else:
-        #     print(1, end=" ")
     print()
