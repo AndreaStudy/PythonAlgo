80698bcec332006f05b3c77d56f74a65dbff6f87
Andrea <wjdgnstjr45@gmail.com>
2023-05-07 17:27:54 +0900

    20230507

--- programmers/전력망둘로나누기/sol.py
+++ programmers/전력망둘로나누기/sol.py
@@ -1,3 +1,35 @@
+INF = int(1e9)
+
+def DFS(v, graph, visited, check_link):
+    cnt = 1
+    visited[v] = 1
+
+    for adj_v in graph[v]:
+        if not visited[adj_v] and check_link[v][adj_v]:
+            cnt += DFS(adj_v, graph, visited, check_link)
+
+    return cnt
+
+
 def solution(n, wires):
-    answer = -1
+    answer = INF
+
+    check_link = [[1]*(n+1) for _ in range(n+1)]
+
+    graph = [[] for _ in range(n+1)]
+
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
+
+    for a, b in wires:
+        visited = [0]*(n+1)
+
+        check_link[a][b] = 0
+        cnt_a = DFS(a, graph, visited, check_link)
+        cnt_b = DFS(b, graph, visited, check_link)
+        check_link[a][b] = 1
+
+        answer = min(answer, abs(cnt_a - cnt_b))
+
     return answer
