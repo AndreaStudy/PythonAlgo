68e471fb8a53d6b9db76ea93e7606aeb54885252
Andrea <wjdgnstjr45@gmail.com>
2024-05-30 22:59:45 +0900

    20240530 baekjoon/s3/1002

---
+++ baekjoon/silver/s3_1002.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+N = int(input())
+
+for n in range(N) :
+    x1, y1, r1, x2, y2, r2 = list(map(int, input().split()))
+    
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    
+    if distance == 0 :
+        if r1 == r2 :
+            print(-1)
+        else :
+            print(0)
+    else :
+        if r1 + r2 == distance or abs(r1 - r2) == distance :
+            print(1)
+        elif ((abs(r1 - r2) < distance) and (distance < r1 + r2)) :
+            print(2)
+        else :
+            print(0)
+    
