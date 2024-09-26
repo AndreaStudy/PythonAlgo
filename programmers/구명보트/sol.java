bea85e69bbcea6e630cef207779ffb5944bbc835
Andrea <wjdgnstjr45@gmail.com>
2024-09-26 20:47:55 +0900

    20240926 programmers 구명보트 투포인터 활용 java, python

---
+++ programmers/구명보트/sol.java
@@ -0,0 +1,23 @@
+import java.util.*
+
+public class Solution {
+
+  public int solution(int[] people, int limit) {
+    int answer = 0;
+
+    Arrays.sort(people);
+
+    int min = 0;
+    int max = people.length -1;
+
+    while (min < max) {
+      if (people[min] + people[max] <= limit) {
+        min++;
+        answer ++;
+      }
+      max--;
+    }
+
+    return people.length-answer;
+  }
+}
