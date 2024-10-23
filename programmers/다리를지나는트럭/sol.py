5d3db6f96338d180a1eb198eff1b837f60da872f
Andrea <wjdgnstjr45@gmail.com>
2024-10-23 23:59:52 +0900

    20241023 programmers 다리를지나는 트럭 deque

---
+++ programmers/다리를지나는트럭/sol.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+  answer = 0
+  bridge = deque([0] * bridge_length)
+  truck_weights = deque(truck_weights)
+  
+  currentWeight = 0
+  while truck_weights:
+    answer += 1
+    currentWeight -= bridge.popleft()
+    
+    if currentWeight + truck_weights[0] <= weight:
+      currentWeight += truck_weights[0]
+      bridge.append(truck_weights.popleft())
+    else:
+      bridge.append(0)
+      
+  answer += bridge_length
+  
+  return answer
